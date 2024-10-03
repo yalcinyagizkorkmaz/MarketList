e629fa88e36fa6f9651bf11c542032a0fada91e6
Yalçın Yağız KORKMAZ <99255561+yalcinyagizkorkmaz@users.noreply.github.com>
2024-10-03 16:14:03 +0300

    login-sign-up-butonları

--- my-app/src/Component/GirisSayfa.js
+++ my-app/src/Component/GirisSayfa.js
@@ -116,12 +116,20 @@ const GirisSayfa = () => {
             )}
           </div>
           <div className="flex items-center justify-between">
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="submit"
-            >
-              Sign Up
-            </button>
+          <button
+    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-r focus:outline-none focus:shadow-outline"
+    type="button" // Bu login butonuna click event ekleyebilirsin.
+  >
+    Login
+  </button>
+            
+          <button
+    className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-l focus:outline-none focus:shadow-outline"
+    type="submit"
+  >
+    Sign Up
+  </button>
+
           </div>
         </form>
         <p className="text-center text-gray-500 text-xs">
